4ccd52cc735e42214216445da51f4418de5c7d95
Ianqt01 <ianmarkturno123@gmail.com>
2026-08-27 15:11:34 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Paste your deployed Google Apps Script Web App URL here.
-const API_URL = "https://script.google.com/macros/s/AKfycbxhu4qm3YVa8pIfh89HLP6gYYxIouP3swRFKvEarGWrElemmj71s3HVDH6LGzfTt_RS/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbyFAqZvisW4MTkSxI_uFBb2SzBu1oG5G1UT2vBH2qv1ttIDSPtxOv87Bn8cmFUSubo6/exec";
